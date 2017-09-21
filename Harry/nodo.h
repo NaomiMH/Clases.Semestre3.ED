644ce402a8896f1407a32320dce90328044c047c
Naomi M. Honti <naomiperla.macias@hotmail.com>
2017-09-20 21:56:48 -0500

    Clase

---
+++ Harry/nodo.h
@@ -0,0 +1,17 @@
+#ifndef NODO_H
+#define NODO_H
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class nodo
+{
+    public:
+        T dato;
+        nodo <T> *sig;
+        nodo(){sig=NULL;}
+        nodo(T d){dato=d;sig=NULL;}
+};
+
+#endif // NODO_H
